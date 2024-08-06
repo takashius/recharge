425c24f8bcb69bf2c3c1f95e20402a32b0a9380f
Erick Hernandez <takashi.onimaru@gmail.com>
2024-08-05 21:50:21 -0400

    Material design UI Installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { routes } from './routes'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import '../i18n';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
