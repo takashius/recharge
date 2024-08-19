29a7c164f7dcf870d7196297690f1d0be8d3f653
Erick Hernandez <takashi.onimaru@gmail.com>
2024-08-19 17:50:07 -0400

    "Updated vite.config.ts: added build options, reformatted code, and changed quotes from single to double"

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
-  ],
+  plugins: [react({ include: /\.(mdx|js|jsx|ts|tsx)$/ })],
+  build: {
+    rollupOptions: {
+      external: [
+        "@fortawesome/fontawesome-svg-core",
+        "@fortawesome/free-solid-svg-icons",
+        "@fortawesome/free-brands-svg-icons",
+      ],
+    },
+  },
   resolve: {
     alias: {
-      "src": path.resolve(__dirname, "./src"),
-      "mocks": path.resolve(__dirname, "./mocks"),
+      src: path.resolve(__dirname, "./src"),
+      mocks: path.resolve(__dirname, "./mocks"),
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
-})
+});
