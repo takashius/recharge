275bca3b2c4ea98c432e09d1a1d7c96568ac8c95
Erick Hernandez <takashi.onimaru@gmail.com>
2024-08-19 18:15:28 -0400

    "Update tsconfig.app.json and vite.config.ts: modify compiler options and rollup output"

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,11 @@ export default defineConfig({
   plugins: [react({ include: /\.(mdx|js|jsx|ts|tsx)$/ })],
   build: {
     rollupOptions: {
-      output: {
-        globals: {
-          "@fortawesome/fontawesome-svg-core": "FontAwesome",
-          "@fortawesome/free-solid-svg-icons": "FontAwesomeSolid",
-          "@fortawesome/free-brands-svg-icons": "FontAwesomeBrands",
-        },
-      },
+      external: [
+        "@fortawesome/fontawesome-svg-core",
+        "@fortawesome/free-solid-svg-icons",
+        "@fortawesome/free-brands-svg-icons",
+      ],
     },
   },
   resolve: {
